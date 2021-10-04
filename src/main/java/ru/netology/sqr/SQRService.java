12e83471526db3db91efcdba41ab4db72deb38b1
leonidbeltsov <leonidbeltsov@gmail.com>
2021-10-04 22:51:57 +0400

    second commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,8 +4,9 @@ public class SQRService {
     public int calculate(int lowerBound, int upperBound) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
+            int j = i * i;
 //            Если квадрат i входит в границы диапазона lowerBound-upperBound счетчик count увеличивается на еденицу.
-            if (i * i >= lowerBound && i * i <= upperBound) {
+            if (j >= lowerBound && j <= upperBound) {
                 count++;
             }
         }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,13 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "200, 300, 3",
+    @CsvSource(value = {"200, 300, 3",
             "200, 400, 6",
             "-99, 99, 0",
             "9000, 10001, 5"
     })
-    void calculate(int lowerBound, int upperBound, int expected) {
+    void shouldFindSumSquares(int lowerBound, int upperBound, int expected) {
         SQRService service = new SQRService();
         int actual = service.calculate(lowerBound, upperBound);
         assertEquals(expected, actual);
