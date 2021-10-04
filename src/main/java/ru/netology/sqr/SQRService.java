cb05ed5db945bbdde6c69fdb5ae338cec21962b9
leonidbeltsov <leonidbeltsov@gmail.com>
2021-10-04 16:30:56 +0400

    add comment

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,6 +4,7 @@ public class SQRService {
     public int calculate(int lowerBound, int upperBound) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
+//            Если квадрат i входит в границы диапазона lowerBound-upperBound счетчик count увеличивается на еденицу.
             if (i * i >= lowerBound && i * i <= upperBound) {
                 count++;
             }
